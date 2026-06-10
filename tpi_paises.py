9ffd473c6d5309583ef7b772dd02feb3bccfd53e
nipponpark <nipponpark@hotmail.com>
2026-06-10 10:55:15 -0300

    Cargo la función agregar_paises

--- tpi_paises.py
+++ tpi_paises.py
@@ -31,7 +31,90 @@ def cargar_paises():
 #FUNCIONES PRINCIPALES
 #AGREGAR_PAISES
 def agregar_pais(paises):
-    pass
+    print("\n--- AGREGAR UN PAÍS ---")
+
+    nombre_agregar = input("Ingrese el nombre del país que quiera agregar: ").strip()
+    
+    if nombre_agregar == "":
+        print("El nombre no puede estar vacío.")
+        return
+    
+
+    if pais_duplicado(paises, nombre_agregar):
+        print("Este país ya existe. ")
+        return
+    
+    while True:
+        try:
+            agregar_pob = int(input("Ingrese la población del país ingresado: "))
+
+            if agregar_pob <= 0:
+                print("La población debe ser mayor a 0.")
+            else:
+                break
+        except ValueError:
+            print("Debe ingresar un número entero. ")    
+
+    while True:
+        try:
+            agregar_sup = int(input("Ingrese la superficie del páís ingresado: "))
+
+            if agregar_sup <= 0:
+                print("La superficie debe ser mayor a 0.")
+            else:
+                break
+        except ValueError:
+            print("Debe ingresar un número entero. ")  
+
+
+    
+    while True:
+        print("Seleccione el continente del país ingresado")
+        print("""
+1. America
+2. Europa
+3. Asia
+4. Africa
+5. Oceania
+""")
+        try:
+            opcion = int(input("Seleccione un continente: "))
+            
+            if opcion == 1:
+                continente = "America"
+                break
+
+            elif opcion == 2:
+                continente = "Europa"
+                break
+
+            elif opcion == 3:
+                continente = "Asia"
+                break
+
+            elif opcion == 4:
+                continente = "Africa"
+                break
+
+            elif opcion == 5:
+                continente = "Oceania"     
+                break
+
+            else:
+                print("Opción inválida. ")
+        except ValueError:
+            print("Debe ingresar un número válido. ")     
+
+    nuevo_pais = {
+    "nombre": nombre_agregar,
+    "poblacion": agregar_pob,
+    "superficie": agregar_sup,
+    "continente": continente}   
+
+    paises.append(nuevo_pais)
+
+    print("País agregado correctamente.")
+
 
 #ACTUALIZAR_PAISES
 def actualizar_pais(paises):
@@ -54,7 +137,7 @@ def actualizar_pais(paises):
     try:
         nueva_pob = int(input("Nueva población: "))
         nueva_sup = int(input("Nueva superficie (km²): "))
-        if nueva_pob < 0 or nueva_sup < 0:
+        if nueva_pob <= 0 or nueva_sup <= 0:
             print("Error: Valores negativos no permitidos.")
             return
     except ValueError:
@@ -68,7 +151,31 @@ def actualizar_pais(paises):
 
 #BUSCAR_PAIS
 def buscar_pais(paises):
-    pass
+    print("\n--- BUSCAR PAÍS ---")
+
+    buscar_nombre = input("Ingrese el nombre del país a buscar: ").strip()
+
+    if buscar_nombre == "":
+        print("Debe ingresar un nombre.")
+        return
+
+    pais_encontrado = False
+
+    for pais in paises:
+
+        if buscar_nombre.lower() in pais["nombre"].lower():
+
+            print(f"""
+Nombre: {pais["nombre"]}
+Población: {pais["poblacion"]}
+Superficie: {pais["superficie"]} km²
+Continente: {pais["continente"]}
+""")
+
+            pais_encontrado = True
+
+    if pais_encontrado == False:
+        print("No se encontró ningún país.")
 
 
 ##############################################
@@ -219,11 +326,18 @@ def cantidad_por_continente(paises):
 
 
 ##############################################
-#FUNCION_EXTRA
+#FUNCIONES_ADICIONALES
 def mostrar_paises(paises): 
     print("\n =====PAISES=====")
     for pais in paises:
         print(pais["nombre"], "-", pais["continente"])
+
+def pais_duplicado(paises, nombre):
+    for pais in paises:
+        if pais["nombre"].lower == nombre.lower():
+            return True
+
+    return False            
 
 
 ##############################################
@@ -256,13 +370,13 @@ SISTEMA DE GESTION DE PAISES
             opcion = int(input("Seleccione una opción: "))
 
             if opcion == 1:
-                print("test op1")
+                agregar_pais(paises)
 
             elif opcion == 2:
-               print("test op2")
+               actualizar_pais(paises)
 
             elif opcion == 3:
-                print("test op3")
+                buscar_pais(paises)
 
             elif opcion == 4:
                 filtrar_paises(paises)
@@ -271,7 +385,7 @@ SISTEMA DE GESTION DE PAISES
                 ordenar_paises(paises)
 
             elif opcion == 6:
-                print("test op 6")
+                mostrar_estadisticas(paises)
 
             elif opcion == 7:
                 mostrar_paises(paises)
